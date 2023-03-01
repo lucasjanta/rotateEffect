4cab5cf687ba109c683b44f79009e11f5b34af8e
lucasjanta <lucastyle2014@gmail.com>
2023-03-01 11:57:01 -0300

    Animation fix

--- code.js
+++ code.js
@@ -1,19 +1,30 @@
 var nomefundo = document.querySelector(".nomefundo");
 var nomemeio = document.querySelector(".nomemeio");
 
-const largura = window.innerWidth;
-
-console.log(largura);
-
 document.addEventListener("mousemove", (movimento)=>{
+    var largura = window.innerWidth;
     var movimentofundo = movimento.pageX;
     var porcentagemMouse = (movimentofundo * 100) / largura;
-    if (porcentagemMouse > 50) {
-        gsap.to(".nomefundo", {rotation: 5, duration: 2});
-        gsap.to(".nomemeio", {rotation: 5, duration: 1});
-    } else {
-        gsap.to(".nomefundo", {rotation: -5, duration: 2});
-        gsap.to(".nomemeio", {rotation: -5, duration: 1});
-    }
+    if (porcentagemMouse > 70) {
+        //Direitaif (porcentagemMouse > 70) {
+            rotationVar = 5;
+        } 
+        else if (porcentagemMouse < 30) {
+            rotationVar = -5;
+        }
+        else {
+            rotationVar = 0;
+        }
+        // gsap.to(".nomefundo", {rotation: 5, duration: 2});
+        // gsap.to(".nomemeio", {rotation: 5, duration: 1});
+        // gsap.to(".imagemgon", {rotation: 3, duration: 0.5});
+        //Esquerda
+        // gsap.to(".nomefundo", {rotation: -5, duration: 2});
+        // gsap.to(".nomemeio", {rotation: -5, duration: 1});
+        // gsap.to(".imagemgon", {rotation: -3, duration: 0.5});
+    gsap.to(".nomefundo", {rotation: rotationVar, duration: 2});
+    gsap.to(".nomemeio", {rotation: rotationVar, duration: 1});
+
+
 })
 
